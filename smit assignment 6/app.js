a95e071363d2ccd8f44317541fb5d54f74f4815c
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-03-11 20:06:41 +0500

    add 10 questions

---
+++ smit assignment 6/app.js
@@ -0,0 +1,98 @@
+// // chapter no 21 - 25
+
+// // Q no 1
+
+
+// var firstName = prompt("ENTER YOUR FIRST NAME")
+// var lastName = prompt("ENTER LAST YOUR NAME")
+
+// var fullName = firstName + " " + lastName
+
+// console.log(fullName)
+
+
+// // Q no 2
+
+// var userMob = prompt("Enter your fav mob")
+
+// var result = userMob.length
+
+// console.log(result)
+
+
+
+// // Q no 3
+
+// var con = "pakistan"
+
+
+// var result = con.indexOf("n")
+
+// console.log(result)
+
+
+// // Q no 4
+
+// var hel = "hello world"
+
+// var result = hel.lastIndexOf("l")
+
+// console.log(result)
+
+
+// // Q no 5
+
+
+// var city = "karachi"
+
+// var result = city.charAt(3)
+
+// console.log(result)
+
+
+// // Q no 6
+
+
+// var firstName = prompt("ENTER YOUR FIRST NAME")
+// var lastName = prompt("ENTER LAST YOUR NAME")
+
+// var fullName = firstName.concat( " " + lastName)
+
+// console.log(fullName)
+
+
+// //  Q no 7
+
+// var orSt = "Hyderabad"
+
+// var result = orSt.replace("Hyder","islam")
+
+// console.log(result)
+
+
+// // Q no 8
+
+
+// var massege = "Ali and Sami are best friends. They play cricket and football together."
+
+// var result = massege.replaceAll("and", "&")
+
+// console.log(result)
+
+
+// // Q no 9
+ 
+// var num = +"456"
+
+// console.log(num)
+
+
+// Q no 10
+
+// var item = "peanut"
+
+// var res = item.toLocaleUpperCase()
+// console.log(res)
+
+
+
